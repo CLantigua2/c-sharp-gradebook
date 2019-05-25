f3ce139d7a3bc835499a50a86640f7ab6fc48b0f
Carlos Lantigua <clantigua954@gmail.com>
2019-05-25 12:51:52 -0400

    adding init project

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+namespace GradeBook
+{
+    class Book
+    {
+        // constructor
+        public Book(string name)
+        {
+            grades = new List<double>();
+            this.name = name;
+        }
+
+
+        public void AddGrade(double grade)
+        {
+            this.grades.Add(grade);
+        }
+
+        public void showStatistics()
+        {
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+            var result = 0.0;
+            foreach (var number in this.grades)
+            {
+                lowGrade = Math.Min(number, lowGrade);
+                highGrade = Math.Max(number, highGrade);
+                result += number;
+            }
+            result /= grades.Count;
+            Console.WriteLine(value: $"\nThe average grade is {result:N1}\n The highest grade is {highGrade:N1}\n The lowest grade is {lowGrade:N1}");
+        }
+        // this is a field because it's outside of a method
+        // made private so that only this class can have access to grades
+        private List<double> grades;
+        private string name;
+    }
+}
---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Carlos's Grade Book");
+            book.AddGrade(89.1);
+            book.AddGrade(90.4);
+            book.AddGrade(101);
+            book.showStatistics();
+            // creating a new array of numbers
+            // double[] numbers = new double[3] { 12.7, 5.2, 1.3 };
+            // double[] numbers = new[] { 12.7, 5.2, 1.3 };
+            // var result = 0.0;
+            // List<double> grades = new List<double>() { 12.7, 5.2, 1.3 };
+            // grades.Add(56.1);
+
+        }
+    }
+}
